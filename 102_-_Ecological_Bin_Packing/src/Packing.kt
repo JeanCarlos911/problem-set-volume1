4395f2a2b4a801af8ca6d9993ab534a164a9b3b7
JeanCarlos911 <jeancarlosodi@gmail.com>
2020-09-14 19:33:41 -0500

    falta solucionar noSuchElementException

---
+++ 102_-_Ecological_Bin_Packing/src/Packing.kt
@@ -0,0 +1,104 @@
+import java.io.InputStream
+import java.util.Scanner
+
+fun main() {
+    val input = Scanner(InputStream.nullInputStream())
+
+    val packing1 = Packing(input)
+    val packing2 = Packing(input)
+    val packing3 = Packing(input)
+
+    var total = 0
+    var min = 0
+    val combinations = ArrayList<String>()
+
+    for(a in 1..3){
+        for(b in 1..3){
+            for(c in 1..3){
+                if(a != b && b != c) {
+                    total = when(a) {
+                        1 -> packing2.brownBottles + packing3.brownBottles
+                        2 -> packing2.greenBottles + packing3.greenBottles
+                        else -> packing2.clearBottles + packing3.clearBottles
+                    } + when(b) {
+                        1 -> packing1.brownBottles + packing3.brownBottles
+                        2 -> packing1.greenBottles + packing3.greenBottles
+                        else -> packing1.clearBottles + packing3.clearBottles
+                    } + when(c) {
+                        1 -> packing1.brownBottles + packing2.brownBottles
+                        2 -> packing1.greenBottles + packing2.greenBottles
+                        else -> packing1.clearBottles + packing2.clearBottles
+                    }
+                    if(combinations.size == 0) {
+                        min = total
+                        combinations.add({
+                            when (a) {
+                                1 -> "B"
+                                2 -> "G"
+                                else -> "C"
+                            } + when (b) {
+                                1 -> "B"
+                                2 -> "G"
+                                else -> "C"
+                            } + when (c) {
+                                1 -> "B"
+                                2 -> "G"
+                                else -> "C"
+                            }
+                        }.toString())
+                    }else if (min < total) {
+                        min = total
+                        combinations.removeAll(combinations)
+                        combinations.add({
+                            when (a) {
+                                1 -> "B"
+                                2 -> "G"
+                                else -> "C"
+                            } + when (b) {
+                                1 -> "B"
+                                2 -> "G"
+                                else -> "C"
+                            } + when (c) {
+                                1 -> "B"
+                                2 -> "G"
+                                else -> "C"
+                            }
+                        }.toString())
+                    }else if (min == total) {
+                        combinations.add({
+                            when (a) {
+                                1 -> "B"
+                                2 -> "G"
+                                else -> "C"
+                            } + when (b) {
+                                1 -> "B"
+                                2 -> "G"
+                                else -> "C"
+                            } + when (c) {
+                                1 -> "B"
+                                2 -> "G"
+                                else -> "C"
+                            }
+                        }.toString())
+                    }
+                }
+            }
+        }
+
+        combinations.sort()
+        println("${combinations.get(0)} $min")
+    }
+}
+
+class Packing {
+
+    var brownBottles = 0
+    var greenBottles = 0
+    var clearBottles = 0
+
+    constructor (input : Scanner){
+        brownBottles = input.nextInt()
+        greenBottles = input.nextInt()
+        clearBottles = input.nextInt()
+    }
+}
