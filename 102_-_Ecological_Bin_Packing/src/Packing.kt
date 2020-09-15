0392f01e2dbbccbfebf6cad906060f6de8dce3dc
JeanCarlos911 <jeancarlosodi@gmail.com>
2020-09-14 21:34:41 -0500

    added a bit of interface

--- 102_-_Ecological_Bin_Packing/src/Packing.kt
+++ 102_-_Ecological_Bin_Packing/src/Packing.kt
@@ -1,6 +1,73 @@
+import java.awt.Color
 import java.util.Scanner
+import javax.swing.JFrame
+import javax.swing.JPanel
+import javax.swing.JTextField
 
 fun main() {
+    val frame = JFrame()
+
+    val panel1 = JPanel()
+    panel1.setSize(384, 624)
+    panel1.setLocation(32, 32)
+    panel1.setLayout(null)
+    panel1.setBackground(Color(80, 80, 80))
+    frame.add(panel1)
+
+    val textField1 = JTextField()
+    textField1.setText("Brown bottles")
+    textField1.setSize(85, 32)
+    textField1.setLocation(32, 560)
+    panel1.add(textField1)
+
+    val textField2 = JTextField()
+    textField2.setText("Green bottles")
+    textField2.setSize(100, 32)
+    textField2.setLocation(149, 560)
+    panel1.add(textField2)
+
+    val textField3 = JTextField()
+    textField3.setText("Clear bottles")
+    textField3.setSize(100, 32)
+    textField3.setLocation(328, 568)
+    frame.add(textField3)
+
+    val textField4 = JTextField()
+    textField4.setText("Brown bottles")
+    textField4.setSize(100, 32)
+    textField4.setLocation(460, 568)
+    frame.add(textField4)
+
+    val textField5 = JTextField()
+    textField5.setText("Green bottles")
+    textField5.setSize(100, 32)
+    textField5.setLocation(592, 568)
+    frame.add(textField5)
+
+    val textField6 = JTextField()
+    textField6.setText("Clear bottles")
+    textField6.setSize(100, 32)
+    textField6.setLocation(724, 568)
+    frame.add(textField6)
+
+    val textField7 = JTextField()
+    textField7.setText("Brown bottles")
+    textField7.setSize(100, 32)
+    textField7.setLocation(856, 568)
+    frame.add(textField7)
+
+    val textField8 = JTextField()
+    textField8.setText("Green bottles")
+    textField8.setSize(100, 32)
+    textField8.setLocation(988, 568)
+    frame.add(textField8)
+
+    val textField9 = JTextField()
+    textField9.setText("Clear bottles")
+    textField9.setSize(100, 32)
+    textField9.setLocation(1120, 568)
+    frame.add(textField9)
+
     val input = Scanner(System.`in`)
 
     val packing1 = Packing(input)
@@ -86,6 +153,14 @@ fun main() {
     }
     combinations.sort()
     println("${combinations.get(0)} $min")
+
+    frame.setSize(1280, 720)
+    frame.setLocationRelativeTo(null)
+    frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE)
+    frame.setLayout(null)
+    frame.getContentPane().setBackground(Color.BLACK)
+    frame.setVisible(true)
+
 }
 
 class Packing {
