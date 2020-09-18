680a8cb1144cdf4607ad843635debc1a3eddad66
JeanCarlos911 <jeancarlosodi@gmail.com>
2020-09-18 07:28:59 -0500

    translated to english

--- 100_-_The_3n_+_1_problem/src/App.java
+++ 100_-_The_3n_+_1_problem/src/App.java
@@ -1,21 +1,18 @@
 import javax.swing.JOptionPane;
 
 public class App{
-    private int minimo, maximo, resultado;
-    private final String[] options = {"Si", "No"};
+    private int min, max, answer;
+    private final String[] options = {"Yes", "No"};
     
     public static void main(String[] args){
         App app = new App();
     }
     
     public App(){
-        ejecutar();
-    }
-    
-    private void ejecutar(){
-        recibirValores();
-        evaluar();
-        int x = JOptionPane.showOptionDialog(null, "¿Desea ejecutar de nuevo?", "Principal", JOptionPane.DEFAULT_OPTION, JOptionPane.QUESTION_MESSAGE,
+        receiveValues();
+        calculate();
+        showAnswer();
+        int x = JOptionPane.showOptionDialog(null, "Do you want to run again?", "Main", JOptionPane.DEFAULT_OPTION, JOptionPane.QUESTION_MESSAGE,
                 null, options, options[0]);
         switch(x){
             case 0:
@@ -25,55 +22,49 @@ public class App{
                 System.exit(0);
         }
     }
-    
-    private void recibirValores(){
+
+    private void receiveValues(){
         do{
-            minimo = Integer.parseInt(JOptionPane.showInputDialog("Ingrese i"));
-            if(minimo<1 || minimo>9999){
-                JOptionPane.showMessageDialog(null, "Valor insertado no válido", "Error", 0);
+            min = Integer.parseInt(JOptionPane.showInputDialog("Enter i"));
+            if(min <1 || min >9999){
+                JOptionPane.showMessageDialog(null, "Invalid inserted value", "Error", JOptionPane.ERROR_MESSAGE);
             }else
                 break;
         }while(true);
         
         do{
-            maximo = Integer.parseInt(JOptionPane.showInputDialog("Ingrese j"));
-            if(maximo<1 || maximo>10000 || minimo>maximo){
-                JOptionPane.showMessageDialog(null, "Valor insertado no válido", "Error", 0);
+            max = Integer.parseInt(JOptionPane.showInputDialog("Enter j"));
+            if(max <1 || max >10000 || min > max){
+                JOptionPane.showMessageDialog(null, "Invalid inserted value", "Error", JOptionPane.ERROR_MESSAGE);
             }else
                 break;
         }while(true);
     }
-    
-    private void evaluar(){
-        calcularResultado();
-        mostrarResultado();
-    }
-    
-    private void calcularResultado(){
-        resultado = 0;
-        int n, contador;
+
+    private void calculate(){
+        answer = 0;
+        int n, cont;
         
-        for(int i=minimo; i<=maximo; i++){
+        for(int i = min; i<= max; i++){
             n = i;
-            contador=0;
-            while(n!=1){
-                if(n%2==0){
-                    n/=2;
-                    contador+=1;
+            cont = 0;
+            while(n != 1){
+                if(n%2 == 0){
+                    n /= 2;
+                    cont += 1;
                 }else{
-                    n=3*n+1;
-                    contador+=1;
+                    n = 3*n + 1;
+                    cont+ = 1;
                 }
             }
-            contador+=1;
-            if(contador>resultado){
-                resultado=contador;
-            }
+            cont += 1;
+            if(cont > answer)
+                answer = cont;
         }
     }
     
-    private void mostrarResultado(){
-        JOptionPane.showMessageDialog(null, String.format("%d %d %d", minimo, maximo, resultado), "Output", JOptionPane.INFORMATION_MESSAGE);
+    private void showAnswer(){
+        JOptionPane.showMessageDialog(null, String.format("%d %d %d", min, max, answer), "Output", JOptionPane.INFORMATION_MESSAGE);
     }
     
 }
