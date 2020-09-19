5962d6d2cb4b5690b570e1671a33c1eb3d944017
JeanCarlos911 <jeancarlosodi@gmail.com>
2020-09-19 13:25:27 -0500

    added mainBar

--- 104_-_Arbitrage/src/Main.kt
+++ 104_-_Arbitrage/src/Main.kt
@@ -7,6 +7,7 @@ fun main() {
  *  hacer que se mueva la ventana con el mouse :D
  *  añadir scrollPane al JTextArea
  *  implementar el metodo calculate
+ *  añadir sombra a la ventana
  */
 fun calculate(str: String): String{
     return str
--- 104_-_Arbitrage/src/Window.kt
+++ 104_-_Arbitrage/src/Window.kt
@@ -1,4 +1,5 @@
 import services.graphicService.*
+import services.graphicService.components.mainBar.MainBar
 import java.awt.event.MouseEvent
 import java.awt.event.MouseListener
 import javax.swing.*
@@ -8,44 +9,46 @@ class Window(title: String) : JFrame(), MouseListener {
     private val runBox = JTextArea()
     private val resultBox = JTextArea()
 
+    private val iLogo = ImageIcon("resources/logo.png")
     private val iBtRunOn = ImageIcon("resources/btRunOn.png")
     private val iBtRunOff = ImageIcon("resources/btRunOff.png")
-    private val iBtExitOn = ImageIcon("resources/btExitOn.png")
-    private val iBtExitOff = ImageIcon("resources/btExitOff.png")
     private val iBtCleanOn = ImageIcon("resources/btCleanOn.png")
     private val iBtCleanOff = ImageIcon("resources/btCleanOff.png")
 
     private val btRun = JButton()
-    private val btExit = JButton()
     private val btClean = JButton()
 
     init {
         isUndecorated = true
 
+        val mainBar = MainBar(1280)
+        mainBar.setLogo(iLogo)
+        add(mainBar)
+
         val pLeft = JPanel()
-        pLeft.setProperties(32, 100, 568, 564, white, null, blackBorder)
+        pLeft.setProperties(32, 100, 568, 564, megaDarkGray, null, semiDarkGray2Border)
         add(pLeft)
 
         val pRight = JPanel()
-        pRight.setProperties(664, 100, 569, 564, white, null, blackBorder)
+        pRight.setProperties(664, 100, 569, 564, megaDarkGray, null, semiDarkGray2Border)
         add(pRight)
 
         val lTitle = JLabel()
-        lTitle.setProperties(0, 7, 1280, 32, title, red, fontTitleProblem, "CENTER")
+        lTitle.setProperties(0, 32, 1280, 32, title, darkWhite, fontTitleProblem, "CENTER")
         add(lTitle)
 
         val lIn = JLabel()
-        lIn.setProperties(50, 50, 400, 32, "IN", red, fontTitleProblem)
+        lIn.setProperties(50, 50, 400, 32, "IN", darkWhite, fontTitleProblem)
         add(lIn)
 
         val lOut = JLabel()
-        lOut.setProperties(1150, 50, 400, 32, "OUT", red, fontTitleProblem)
+        lOut.setProperties(1150, 50, 400, 32, "OUT", darkWhite, fontTitleProblem)
         add(lOut)
 
-        runBox.setProperties(7, 7, 558, 554)
+        runBox.setProperties(7, 7, 558, 554, null, darkWhite, megaDarkGray, white)
         pLeft.add(runBox)
 
-        resultBox.setProperties(7, 7, 558, 554)
+        resultBox.setProperties(7, 7, 558, 554, null, darkWhite, megaDarkGray, white)
         resultBox.isEditable = false
         pRight.add(resultBox)
 
@@ -57,21 +60,12 @@ class Window(title: String) : JFrame(), MouseListener {
         btClean.addMouseListener(this)
         add(btClean)
 
-        btExit.setProperties(1232, 0, iBtExitOff, defaultCursor)
-        btExit.addMouseListener(this)
-        add(btExit)
-
-        setProperties(1280, 720, null, darkWhite)
+        setProperties(1280, 720, null, semiDarkGrayBlue)
     }
 
     override fun mouseClicked(e: MouseEvent?) {
         if(e?.source == btRun)
             resultBox.text = calculate(runBox.text)
-
-        if(e?.source == btExit) {
-            JOptionPane.showMessageDialog(null, "Application closed successfully", "Exit", JOptionPane.INFORMATION_MESSAGE)
-            exitProcess(0)
-        }
 
         if(e?.source == btClean)
             resultBox.text = ""
@@ -87,9 +81,6 @@ class Window(title: String) : JFrame(), MouseListener {
         if(e?.source == btRun)
             btRun.icon = iBtRunOn
 
-        if(e?.source == btExit)
-            btExit.icon = iBtExitOn
-
         if(e?.source == btClean)
             btClean.icon = iBtCleanOn
     }
@@ -97,9 +88,6 @@ class Window(title: String) : JFrame(), MouseListener {
     override fun mouseExited(e: MouseEvent?) {
         if(e?.source == btRun)
             btRun.icon = iBtRunOff
-
-        if(e?.source == btExit)
-            btExit.icon = iBtExitOff
 
         if(e?.source == btClean)
             btClean.icon = iBtCleanOff
